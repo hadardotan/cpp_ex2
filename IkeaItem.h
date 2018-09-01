9728cc9b10f2711c311b43dd6d0f06182f5370ef
hadardotan <hadardot1@gmail.com>
2018-09-01 21:58:44 +0300

    first commit

---
+++ IkeaItem.h
@@ -0,0 +1,105 @@
+//
+// Created by Visitor on 9/1/2018.
+//
+
+#ifndef CPP_EX2_IKEAITEM_H
+#define CPP_EX2_IKEAITEM_H
+
+#endif //CPP_EX2_IKEAITEM_H
+
+
+#include <string>
+#include <array>
+
+
+/**
+ * constants
+ *
+ */
+#define FURNITURE_DIM 3
+
+
+/**
+ * class represent an item in IKEA store
+ */
+class IkeaItem
+{
+
+
+public:
+
+
+protected:
+
+    int _catalog_number;
+    std::string _item_name;
+    double _price; // price per unit
+
+
+
+};
+
+
+
+class FabricItem : public  IkeaItem
+{
+
+public:
+
+
+protected:
+    double _weight;
+
+};
+
+
+
+
+class CandyItem : public IkeaItem
+{
+
+
+protected:
+
+private:
+    double _calories;
+
+};
+
+
+class LeisureItem : public IkeaItem
+{
+
+protected:
+
+    std::string _author;
+    int _year; //year of publication
+    int _length;
+};
+
+
+
+
+
+class FurnitureItem : public IkeaItem
+{
+
+protected:
+    std::array<double, FURNITURE_DIM> _dimensions;
+
+};
+
+
+//todo - desks and chairs - together???
+
+
+
+class KitchenItem : public FurnitureItem
+{
+
+
+protected:
+    double _capacity;
+};
+
+
---
+++ ikea.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Visitor on 9/1/2018.
+//
+
